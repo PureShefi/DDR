4b800ae6f5a9aa103c6412c7782a1218296519da
Zohar Shefi <wwzohar@gmail.com>
2019-05-02 18:41:29 +0300

    Flappy: Added moving walls

--- FlappyBird/FlappyBird.py
+++ FlappyBird/FlappyBird.py
@@ -1,11 +1,41 @@
 import pygame
 import sys
+import random
+
+SCREEN_SIZE = WIDTH, HEIGHT = (474, 725)
+WALL_DIMENSION = WALL_WIDTH, WALL_HEIGHT = (100, 500)
+GAP = 100
 
 class FlappyBird():
     def __init__(self):
-        self.screen = pygame.display.set_mode((474, 725))
+        self.screen = pygame.display.set_mode(SCREEN_SIZE)
         self.background = pygame.image.load("Assets/background.jpg").convert()
+        self.wallTop = pygame.image.load("Assets/top.png").convert_alpha()
+        self.wallBottom = pygame.image.load("Assets/bottom.png").convert_alpha()
 
+        self.ResetWall()
+        print self.offset
+
+    def MoveWall(self):
+        # Move the wall for the turn
+        self.wallX -= 10
+        if self.wallX <= -WALL_WIDTH:
+            self.ResetWall();
+
+        # Get Center of screen - half of the gap + the offset
+        topLocation = (HEIGHT / 2) - WALL_HEIGHT - (GAP / 2) + self.offset
+        
+        # Get Center of screen + half of the gap + the offset
+        bottomLocation = HEIGHT - (HEIGHT / 2) + (GAP / 2) + self.offset
+
+        self.screen.blit(self.wallTop, (self.wallX, topLocation))
+        self.screen.blit(self.wallBottom, (self.wallX, bottomLocation))
+
+    def ResetWall(self):
+        # Reset the wall to beginning of the screen
+        self.wallX = WIDTH
+        self.offset = random.randint(-150, 100)
+ 
 
     def Run(self):
         clock = pygame.time.Clock()
@@ -19,9 +49,12 @@ class FlappyBird():
                 if (event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN):
                     pass
 
+
             self.screen.fill((255, 255, 255))
             self.screen.blit(self.background, (0, 0))
 
+            self.MoveWall()
+            
             pygame.display.update()
 
 FlappyBird().Run()
